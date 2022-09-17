92ead68e3fd4ab6ae8e31aac2068cdcfaa03f4af
soufiane <01souf01@gmail.com>
2022-09-17 23:43:32 +0100

    first  versin of frontend and backend added

--- src/my-app/src/App.js
+++ src/my-app/src/App.js
@@ -1,76 +1,116 @@
 import { getAllStudents } from './client';
 import { useState, useEffect } from "react";
 import './App.css';
-import { Breadcrumb, Layout, Menu } from 'antd';
 import {
     DesktopOutlined,
     FileOutlined,
     PieChartOutlined,
     TeamOutlined,
     UserOutlined,
+    LoadingOutlined,
 } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, Table, Spin, Empty } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const columns = [
+    {   title: 'Id',
+        dataIndex: 'id',
+        key: 'id',}, {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',}, {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',}, {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',},];
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+
+//spin loader
+const antIcon = (
+    <LoadingOutlined
+        style={{
+            fontSize: 24,
+        }}
+        spin
+    />
+);
 
 function App() {
-
+    //students
     const [students, setStudents] = useState([]);
-    //for the layout
+    //collapsed
     const [collapsed, setCollapsed] = useState(false);
-
-    function getItem(label, key, icon, children) {
-        return {
-            key,
-            icon,
-            children,
-            label,
-        };
-    }
-
-    const items = [
-        getItem('Option 1', '1', <PieChartOutlined />),
-        getItem('Option 2', '2', <DesktopOutlined />),
-        getItem('User', 'sub1', <UserOutlined />, [
-            getItem('Tom', '3'),
-            getItem('Bill', '4'),
-            getItem('Alex', '5'),
-        ]),
-        getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-        getItem('Files', '9', <FileOutlined />),
-    ];
-
-
-    //fetching students
+    //for loading spin
+    const [fetching, setFetching] = useState(true);
+    //fetch students
     const fetchStudents = () =>
         getAllStudents()
             .then(res => res.json())
             .then(data => {
                 console.log(data);
                 setStudents(data);
+                setFetching(false);
     })
 
     useEffect(() => {
-        console.log("compnt is mounted");
         fetchStudents();
 
     },[]);
 
-    if (students.length <= 0){
-        return "no data to show!"
+    const renderStudents = () => {
+        if(fetching){
+            return <Spin indicator={antIcon} />;
+        }
+        if (students.length <= 0){
+            return <Empty>no data</Empty>
+        }
+        return <Table
+            dataSource={students}
+            columns={columns}
+            bordered
+            title={() => 'Students'}
+            rowKey={(student) => student.id}
+        />;
+
     }
-   /* return students.map((student, index) =>{
-        return <p key={index}>
-                              {student.id}
-                              {student.name}
-                              {student.email}
-                              {student.gender}
-                </p>
-    })*/
-    return
-    //////////////
-    <Layout
-        style={{
-            minHeight: '100vh',
-        }}
-    >
+   /* if (students.length <= 0){
+        return "no data to show!"
+    }*/
+     /*return students.map((student, index) =>{
+         return <p key={index}>
+                               {student.id}
+                               {student.name}
+                               {student.email}
+                               {student.gender}
+                 </p>
+     })*/
+
+
+
+    return <Layout style={{minHeight: '100vh',}}>
         <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
             <div className="logo" />
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
@@ -95,25 +135,12 @@ function App() {
                     <Breadcrumb.Item>User</Breadcrumb.Item>
                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
                 </Breadcrumb>
-                <div
-                    className="site-layout-background"
-                    style={{
-                        padding: 24,
-                        minHeight: 360,
-                    }}
-                >
-                    Bill is a cat.
-                </div>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 360,}}>{renderStudents()}</div>
             </Content>
-            <Footer
-                style={{
-                    textAlign: 'center',
-                }}
-            >
-                Ant Design ©2018 Created by Ant UED
-            </Footer>
+            <Footer style={{textAlign: 'center',}}>By soufiane</Footer>
         </Layout>
     </Layout>
+
     /////////////
 }
 
